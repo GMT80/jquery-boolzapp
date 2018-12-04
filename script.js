74e8886e96a0d28c39f78947acec2df0ac7247f0
GMT80 <gianmaria.tarsi@gmail.com>
2018-12-04 19:16:05 +0100

    ..

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 
-
+// apertura jquery
 $(document).ready(function(){
-
+// dall'input dell'utente alla pressione del tasto
   $('#sendMessageInput').on('keypress', function(event) {
-
+// evento della pressione del tasto Invio(13)
       if (event.which == 13) {
+        // console.log(event.which);
+// si recupera il valore dell'input
+        var newMessage = $('#sendMessageInput').val();
+        // console.log(newMessage);
+// si entra nel messaggio vuoto
+        var newMessageTemplate =   $('.template .green_message');
+        // console.log(newMessageTemplate);
+// per pulire i float
+        var clearFix = $('.template .clearfix');
+// inserisce il testo inserito dall'utente
+        newMessageTemplate.text(newMessage);
 
-        var newMessage = $(this).val();
-        console.log(newMessage);
+// si appende nel container principale
+        $('.main_content').append(newMessageTemplate);
+        $('.main_content').append(clearFix);
+// si resetta l'input
+        $('#sendMessageInput').val('');
 
-        $(this).val('');
+        setTimeout(function(){
+
+          var answerMessage = $('.template .white_message');
+
+          $('.main_content').append(answerMessage);
+
+        }, 1500);
+
       };
 
-      $('div.green_message_new').append(newMessage);
-
-      console.log('div.green_message_new');
   });
 
 });
